a38815ab41c82347931a875ab076c7b4a6127a39
Juanmanuel <Juanmanuel@BERROCAL>
2016-04-28 00:20:02 -0400

    init commit

---
+++ interfacesTallPeople/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace interfacesTallPeople
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallguy = new TallGuy() { Name = "Jimmy", Height = 74 };
+            tallguy.TalkAboutYourself();
+            tallguy.Honk();
+        }
+    }
+}
